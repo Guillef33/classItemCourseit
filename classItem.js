a2acc9f6d78b5f7b5fd0bf01d97f54ba1bc95ed0
Guillef33 <guillef33@gmail.com>
2021-02-15 09:38:48 -0300

    classItem

---
+++ classItem.js
@@ -0,0 +1,6 @@
+const wrapper3 = document.getElementById('redWrapper');
+wrapper3.classList.add('redWrapper');
+
+const card3 = document.getElementById('redCard');
+card3.classList.add('redCard');
+
