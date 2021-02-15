558230e2ea837ec0868f30086bd66bac4dcb5ac4
Guillef33 <guillef33@gmail.com>
2021-02-15 10:11:46 -0300

    Uso propiedad style

--- classItem.js
+++ classItem.js
@@ -4,3 +4,12 @@ wrapper3.classList.add('redWrapper');
 const card3 = document.getElementById('redCard');
 card3.classList.add('redCard');
 
+
+// Prueba ajustes de colores con propiedad style
+
+const btn3 = document.getElementById('btn3');
+
+btn3.style.backgroundColor="lightgray"; 
+btn3.style.color="red";
+btn3.style.fontSize="30px";
+
