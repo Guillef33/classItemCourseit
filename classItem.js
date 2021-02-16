3e6cc50f2df8a223bac7c0d83de100bda77f311b
Guillef33 <guillef33@gmail.com>
2021-02-16 09:19:57 -0300

    Ajustes
    Eliminamos !importar
    Eliminamos wrapper
    Definimos width y height en pixeles

--- classItem.js
+++ classItem.js
@@ -1,15 +1,15 @@
 const wrapper3 = document.getElementById('redWrapper');
 wrapper3.classList.add('redWrapper');
 
-const card3 = document.getElementById('redCard');
-card3.classList.add('redCard');
+// const card3 = document.getElementById('redCard');
+// card3.classList.add('redCard');
 
 
 // Prueba ajustes de colores con propiedad style
 
-const btn3 = document.getElementById('btn3');
+//  const btn3 = document.getElementById('btn3');
 
-btn3.style.backgroundColor="lightgray"; 
-btn3.style.color="red";
-btn3.style.fontSize="30px";
+// btn3.style.backgroundColor="lightgray"; 
+// btn3.style.color="red";
+// btn3.style.fontSize="30px";
 
